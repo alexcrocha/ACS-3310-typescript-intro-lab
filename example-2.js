e54c5efc12cf0214467c0d8310cd6b4d37bb6c35
Alex Rocha <rocha.amc@gmail.com>
2023-09-13 10:40:58 -0700

    Solve example 2

--- example-2.js
+++ example-2.js
@@ -1,14 +1,14 @@
 "use strict";
-// Take a look at the code here. Notice the VSCode will 
+// Take a look at the code here. Notice the VSCode will
 // provide errors!
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getPriceWithTax = void 0;
-// Compile the code and check what type script says: 
+// Compile the code and check what type script says:
 // tsc example-2.ts
 function getPriceWithTax(amount, rate) {
-    var price = amount.toFixed(2);
-    var tax = price * rate;
-    return price + tax;
+    var price = amount;
+    var tax = price * (rate / 100);
+    return (price + tax).toFixed(2);
 }
 exports.getPriceWithTax = getPriceWithTax;
 var answer = getPriceWithTax(23.99, 9.5);
