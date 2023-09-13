1dd50f3bf33fd04e80ccbfa57225c449e78b9df0
Alex Rocha <rocha.amc@gmail.com>
2023-09-13 11:13:42 -0700

    Solve examples 7, 8, and 9

--- example-8.ts
+++ example-8.ts
@@ -1,23 +1,33 @@
 // We need a Kaiju class
-// All Kaiju have name, power and type. 
-// Kaiju should have a describe method that 
+// All Kaiju have name, power and type.
+// Kaiju should have a describe method that
 // prints: name:<name> type:<type> power:<power>
 
-// Type can be: lizard, flying, or ape. 
+// Type can be: lizard, flying, or ape.
 // Use an enum to define the type
 
-// Be sure to include a constructor in the 
+// Be sure to include a constructor in the
 // Kaiju class that initializes it's properties!
 
-enum KaijuType { 
-	
+enum KaijuType {
+	Lizard,
+	Flying,
+	Ape
 }
 
 class Kaiju {
-	
+	name: string
+	power: number
+	type: KaijuType
+
+	constructor(name: string, power: number, type: KaijuType) {
+		this.name = name
+		this.power = power
+		this.type = type
+	}
 }
 
 export {
-	Kaiju, 
+	Kaiju,
 	KaijuType
 }
--- example-9.ts
+++ example-9.ts
@@ -5,33 +5,36 @@
 // since monsters are only: lizard, flying, or ape
 
 // Import Kaiju and KaijuType from example-8.js
-
+import { Kaiju, KaijuType } from './example-8'
 // Fix the rampage function so it makes use of the KaijuType enum
 
-function rampage(kaiju, city) {
+function rampage(kaiju: Kaiju, city: string): string {
 	const { name, type, power } = kaiju
 	let action: string
 	switch(type) {
-		case 'ape': 
+		case KaijuType.Ape:
 			action = 'smash'
 			break
 
-		case 'lizard':
+		case KaijuType.Lizard:
 			action = 'burn'
 			break
 
-		case 'flying': 
-			action = 'flap'	
+		case KaijuType.Flying:
+			action = 'flap'
 			break
-	} 
+	}
 	let result = `${name} ${action} over ${city} causing ${power * 10000} damage!`
 	return result
 }
 
-const gojira = new Kaiju('Gojira', 90, KaijuType.lizard)
-// Define two more monsters
+const gojira: Kaiju = new Kaiju('Gojira', 90, KaijuType.Lizard)
+// Define more monsters
+const kingKong: Kaiju = new Kaiju('King Kong', 80, KaijuType.Ape)
+const mothra: Kaiju = new Kaiju('Mothra', 70, KaijuType.Flying)
 
 
 console.log(rampage(gojira, 'tokyo'))
 // Set the new monsts to rampage in a city
-
+console.log(rampage(kingKong, 'san francisco'))
+console.log(rampage(mothra, 'toronto'))
